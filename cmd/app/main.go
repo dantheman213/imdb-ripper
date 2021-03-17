6271f2856267f8c579491bc6fac1afa76ac5c884
Dan Gillespie <daniel.gillespie0949@gmail.com>
2021-03-17 12:29:14 -0700

    working on single threaded

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -47,22 +47,26 @@ var imdbCategoryKeywords []string = []string{
 }
 
 type Movie struct {
-    title      string
-    year       string
-    filmRating string
-    genreList  []string
-    duration   string
-    userRating string
-	description string
+    Title       string   `json:"title"`
+    Year        string   `json:"year"`
+    FilmRating  string   `json:"filmRating"`
+    GenreList   []string `json:"genreList"`
+    Duration    string   `json:"duration"`
+    UserRating  string   `json:"userRating"`
+	Description string   `json:"description"`
 }
 
+var Movies map[string]*Movie
+var count int = 0
+
 func main() {
+	Movies = make(map[string]*Movie)
     ingestMoviesFromIMDB()
     fmt.Println("COMPLETE!")
 }
 
 func generateIMDBURLForKeyword(keyword string, start int) string {
-    return fmt.Sprintf("https://www.imdb.com/search/title/?title_type=movie&genres=%s&start=%d&explore=title_type,genres&ref_=adv_nxt", keyword, start)
+    return fmt.Sprintf("https://www.imdb.com/search/Title/?title_type=movie&genres=%s&start=%d&explore=title_type,genres&ref_=adv_nxt", keyword, start)
 }
 
 func getCategoryItemCount(keyword string) int {
@@ -70,12 +74,11 @@ func getCategoryItemCount(keyword string) int {
 
 	var html *string = nil
 	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
 
 	actions := []chromedp.Action{
 		chromedp.Navigate(ingestionUrl),
 		chromedp.WaitVisible(`div.lister-list`),
-		chromedp.Sleep(523 * time.Millisecond),
+		//chromedp.Sleep(523 * time.Millisecond),
 	}
 
 	// this pre-planned step will get html from DOM
@@ -117,6 +120,7 @@ func getCategoryItemCount(keyword string) int {
 			log.Fatal(err)
 		}
 
+		cancel()
 		return val
 	}
 
@@ -124,113 +128,128 @@ func getCategoryItemCount(keyword string) int {
 }
 
 func ingestMoviesFromIMDB() {
-	movies := make(map[string]*Movie)
-
     for _, keyword := range imdbCategoryKeywords {
-    	count := getCategoryItemCount(keyword)
+    	count = getCategoryItemCount(keyword)
     	fmt.Printf("found %d items in category %s\n\n", count, keyword)
 
         for start := 1; start < count; start += 50 {
-            ingestionUrl := generateIMDBURLForKeyword(keyword, start)
-            fmt.Printf("requesting url: %s", ingestionUrl)
+			if start > 1 && ((start - 1) % 250 == 0) {
+				// status update
+				fmt.Printf("\n\n\n\ncategory %s items processed %d out of %d. %.2f%%\n\n\n\n", keyword, start, count, float64(start)/float64(count))
+				time.Sleep(time.Duration(1) * time.Second)
+			}
+			if start > 1 && ((start - 1) % 5000) == 0 {
+				// save current data structure to disk
+				fmt.Printf("\n\nsaving memory to disk...\n\n")
+				exportToJSON()
+			}
 
-            var html *string = nil
-            ctx, cancel := chromedp.NewContext(context.Background())
-            defer cancel()
-
-            actions := []chromedp.Action{
-                chromedp.Navigate(ingestionUrl),
-                chromedp.WaitVisible(`div.lister-list`),
-                chromedp.Sleep(523 * time.Millisecond),
-            }
-
-            // this pre-planned step will get html from DOM
-            actions = append(actions, chromedp.ActionFunc(func(ctx context.Context) error {
-                node, err := dom.GetDocument().Do(ctx)
-                if err != nil {
-                    fmt.Println(err)
-                    return err
-                }
-
-                data, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
-                if err != nil {
-                    fmt.Println(err)
-                    return err
-                }
-
-                html = &data
-                return err
-            }))
-
-            if err := chromedp.Run(ctx, actions...); err != nil {
-                fmt.Errorf("could not navigate to page: %v", err)
-            }
-
-            // process the HTML here...
-            doc, err := goquery.NewDocumentFromReader(strings.NewReader(*html))
-            if err != nil {
-                log.Println(err)
-                continue
-            }
-
-            if doc != nil {
-                // Find the media items
-                doc.Find(".lister-list .lister-item .lister-item-content").Each(func(i int, s *goquery.Selection) {
-                    // For each item found, get the band and title
-					title := strings.TrimSpace(s.Find(".lister-item-header a").Text())
-
-					year := strings.TrimSpace(s.Find(".lister-item-header span.lister-item-year").Text())
-					year = strings.ReplaceAll(year, "(", "")
-					year = strings.ReplaceAll(year, ")", "")
-					year = strings.ReplaceAll(year, "I", "")
-					year = strings.ReplaceAll(year, "-", "")
-
-					duration := strings.TrimSpace(s.Find("p.text-muted span.runtime").Text())
-					userRating := strings.TrimSpace(s.Find(".ratings-bar strong").Text())
-
-					sel := s.Find("p.text-muted")
-					filmRating := strings.TrimSpace(sel.Eq(0).Find("span.certificate").Text())
-					genreList := strings.Split(strings.TrimSpace(sel.Eq(0).Find("span.genre").Text()), ", ")
-
-					description := strings.TrimSpace(sel.Eq(1).Text())
-
-					movie := Movie{
-						title:      title,
-						year:       year,
-						filmRating: filmRating,
-						genreList:  genreList,
-						duration:   duration,
-						userRating: userRating,
-						description: description,
-					}
-
-					key := fmt.Sprintf("%s|%s", title, year)
-					fmt.Println("KEY:" + key)
-
-					if _, ok := movies[key]; ok {
-						fmt.Printf("skipping because already exists in memory. %s\n")
-						return
-					}
-
-					movies[key] = &movie
-					//fmt.Println(title)
-					//fmt.Println("DESC:" + description)
-					fmt.Printf("items processed: %d\n\n", len(movies))
-                })
-            }
+			ingestMoviePage(keyword, start)
         }
     }
 
-	exportToJSON(movies)
+	exportToJSON()
 }
 
-func exportToJSON(data map[string]*Movie) {
-	bytes, err := json.Marshal(data)
+func ingestMoviePage(keyword string, start int) {
+	ingestionUrl := generateIMDBURLForKeyword(keyword, start)
+	fmt.Printf("requesting url: %s", ingestionUrl)
+
+	var html *string = nil
+	ctx, cancel := chromedp.NewContext(context.Background())
+
+	actions := []chromedp.Action{
+		chromedp.Navigate(ingestionUrl),
+		chromedp.WaitVisible(`div.lister-list`),
+		//chromedp.Sleep(523 * time.Millisecond),
+	}
+
+	// this pre-planned step will get html from DOM
+	actions = append(actions, chromedp.ActionFunc(func(ctx context.Context) error {
+		node, err := dom.GetDocument().Do(ctx)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+
+		data, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+		if err != nil {
+			//fmt.Println(err)
+			return err
+		}
+
+		html = &data
+		return nil
+	}))
+
+	if err := chromedp.Run(ctx, actions...); err != nil {
+		fmt.Errorf("could not navigate to page: %v", err)
+	}
+
+	// process the HTML here...
+	if html != nil {
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(*html))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if doc != nil {
+			// Find the media items
+			doc.Find(".lister-list .lister-item .lister-item-content").Each(func(i int, s *goquery.Selection) {
+				// For each item found, get the band and Title
+				title := strings.TrimSpace(s.Find(".lister-item-header a").Text())
+
+				year := strings.TrimSpace(s.Find(".lister-item-header span.lister-item-year").Text())
+				year = strings.ReplaceAll(year, "(", "")
+				year = strings.ReplaceAll(year, ")", "")
+				year = strings.ReplaceAll(year, "I", "")
+				year = strings.ReplaceAll(year, "-", "")
+
+				duration := strings.TrimSpace(s.Find("p.text-muted span.runtime").Text())
+				userRating := strings.TrimSpace(s.Find(".ratings-bar strong").Text())
+
+				sel := s.Find("p.text-muted")
+				filmRating := strings.TrimSpace(sel.Eq(0).Find("span.certificate").Text())
+				genreList := strings.Split(strings.TrimSpace(sel.Eq(0).Find("span.genre").Text()), ", ")
+
+				description := strings.TrimSpace(sel.Eq(1).Text())
+
+				movie := &Movie{
+					Title:       title,
+					Year:        year,
+					FilmRating:  filmRating,
+					GenreList:   genreList,
+					Duration:    duration,
+					UserRating:  userRating,
+					Description: description,
+				}
+
+				key := fmt.Sprintf("%s|%s", title, year)
+				if _, ok := Movies[key]; ok {
+					fmt.Printf("skipping because already exists in memory. %s\n", key)
+					return
+				}
+
+				fmt.Println("ADDED KEY:" + key)
+				Movies[key] = movie
+			})
+		}
+
+		cancel()
+	} else {
+		fmt.Println("no html in page!")
+	}
+}
+
+func exportToJSON() {
+	bytes, err := json.Marshal(Movies)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	fmt.Printf("writing dataset file with %d bytes...\n", len(bytes))
 	if err := ioutil.WriteFile("/output/dataset.json", bytes, 0644); err != nil {
 		log.Fatal(err)
 	}
